85e78b50c6512235f937c36447e1fbdd94d6598d
songheeseon <82301801+songheeseon@users.noreply.github.com>
2021-07-12 15:25:19 +0900

    Add files via upload

---
+++ continue.js
@@ -0,0 +1,11 @@
+
+    var sum =0;
+    
+
+    for (var i = 0; i<=10; ++i){
+        if (i%2==1)
+            continue
+        sum+=i;
+        document.write(i + "-----------" +sum + "<br>");
+    }
+
